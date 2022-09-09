155c8939c5f5bcd6636543db250a2222de425bd5
EugeneLobski <eugene.lobski@gmail.com>
2022-09-09 20:08:18 +0300

    Add files via upload

---
+++ AnimatorCharacterController2D.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AnimatorCharacterController2D
+{
+    public const string IsMoving = "isMoving";
+    public const string HorizontalMovement = "horizontalMovement";
+    public const string IsRushing = "isRushing";
+}
