84e50adaa3f3ad1c04f07cd2797eb3b2f40e5785
EugeneLobski <eugene.lobski@gmail.com>
2022-09-10 13:26:12 +0300

    Update WaypointMovement.cs

--- WaypointMovement.cs
+++ WaypointMovement.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Animations;
 
 public class WaypointMovement : MonoBehaviour 
 {
